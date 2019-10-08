0ff96a763a46b246e1e58531aeef5ae6fc7b3066
kiret0o0 <52840708+kiret0o0@users.noreply.github.com>
2019-10-08 15:42:49 +0300

    First commit
    No need

--- src/main/java/com/kangelov/Vehicles.java
+++ src/main/java/com/kangelov/Vehicles.java
@@ -7,11 +7,6 @@ public abstract class Vehicles {
     public Vehicles() {
     }
 
-    public int travel(int priceForKilometer, int kilometers){
-        int sum = kilometers * priceForKilometer;
-        System.out.println("Travel is: " + sum);
-        return sum;
-    }
 
 
     public int getDefaultPriceKilometer() {
